6e5fba40331fc55c82f2311ec1c4a35988413736
hahoangphuc20020461 <hahoangphuc1357@gmail.com>
2023-09-28 18:30:15 +0700

    commit

---
+++ btkiemthu2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+double Score(int numberOfCorrect ) {
+return numberOfCorrect * 0.2;
+}
+
+string status(double Score) {
+    string str;
+if (Score < 0) {
+    str = "Error";
+}
+else if (Score < 4) {
+    str = "Kem";
+}
+else if (Score >= 4 && Score <8) {
+    str = "Dat";
+}
+else if (Score >= 8) {
+    str = "Gioi(Award)";
+}
+else {
+    str = "Error";
+}
+return str;
+}
+
+int main() {
+int numberOfCorrect;
+cin >> numberOfCorrect;
+if (numberOfCorrect < 0 || numberOfCorrect > 50) {
+    cout << "Khong hop le";
+}
+else {
+    double sc = Score(numberOfCorrect);
+    string outp = status(sc);
+    cout << outp;
+}
+return 0;
+}
+
